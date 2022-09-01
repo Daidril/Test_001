68f1af660eca476728d067c499c3dbd270264713
Andrei <kadrov2013@yandex.ru>
2022-09-01 19:07:33 +0300

    Example base

--- Example006_Baise/Program.cs
+++ Example006_Baise/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1; //Присваивание переменной a число
+int b = 8; //Присваивание переменной b число
+int c = 3; //Присваивание переменной c число
+int d = 2; //Присваивание переменной d число
+int e = 6; //Присваивание переменной e число
+
+int max = a; //кладем в переменную макс значение переменной a
+
+if(a > max ) max = a; //Если значение a больше макс, то в макс кладем новое значение
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
